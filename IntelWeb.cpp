53e19d9eb6d5b6d5a9e150bf3290ee4298186d6c
Jeannie Chiem <gnyorly@gmail.com>
2016-03-09 18:15:08 -0800

    Update IntelWeb.cpp

--- IntelWeb.cpp
+++ IntelWeb.cpp
@@ -1,16 +1,45 @@
 #include "IntelWeb.h"
+#include "BinaryFile.h"
 
 //////////////////////////////IntelWeb//////////////////////////////////
 
 IntelWeb::IntelWeb() {}
-IntelWeb::~IntelWeb() {}
-bool IntelWeb::createNew(const std::string& filePrefix, unsigned int maxDataItems) {}
-bool IntelWeb::openExisting(const std::string& filePrefix) {}
-void IntelWeb::close() {}
-bool IntelWeb::ingest(const std::string& telemetryFile) {}
+IntelWeb::~IntelWeb() { close(); }
+
+bool IntelWeb::createNew(const std::string& filePrefix, unsigned int maxDataItems) 
+{ 
+	int hashSize = 2700000; // maxDataItems / .75;    //maxDataItems/.75 <= x or less
+
+	dl.createNew(filePrefix + "_dl.dat", hashSize);
+	create.createNew(filePrefix + "_create.dat", hashSize);
+	contact.createNew(filePrefix + "_contact.dat", hashSize);
+}
+
+bool IntelWeb::openExisting(const std::string& filePrefix) 
+{ 
+	close();
+	dl.openExisting(filePrefix + "_dl.dat");
+	create.openExisting(filePrefix + "_create.dat");
+	contact.openExisting(filePrefix + "_contact.dat");
+}
+
+void IntelWeb::close() 
+{
+	dl.close();
+	create.close();
+	contact.close();
+}
+
+bool IntelWeb::ingest(const std::string& telemetryFile) 
+{ return false; }
+
 unsigned int IntelWeb::crawl(const std::vector<std::string>& indicators,
 							 unsigned int minPrevalenceToBeGood,
 							 std::vector<std::string>& badEntitiesFound,
-							 std::vector<InteractionTuple>& badInteractions
-							) {}
-bool IntelWeb::purge(const std::string& entity) {}
+							 std::vector<InteractionTuple>& badInteractions) 
+{
+	return 1;
+}
+
+bool IntelWeb::purge(const std::string& entity) 
+{ return false; }
